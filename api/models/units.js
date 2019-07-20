5478ee0ee7fdf70f523b7fcdbab964557fef7d0f
Colin Griffiths <colinrossgriffiths@gmail.com>
2019-07-19 23:55:18 -0700

    initial units model; adding nodemon sample file; basic file structure.

---
+++ api/models/units.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const unitsSchema = new Schema({
+  kind: {
+    type: String,
+    required: true,
+    enum: [
+      "seat", 
+      "desk", 
+      "small office", 
+      "large office", 
+      "floor"
+    ]    
+  },
+  floor: {
+    type: Number,
+    required: true
+  },
+  special_monthly_offer: {
+    type: Number
+  },
+  company: {
+    type: Array
+  }
+})
+
+module.exports = mongoose.model('Units', unitsSchema)
