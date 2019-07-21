5c0077d04d029c2a06eec45ba77ce8ef6aa285c9
Katrinagd <katrinaguyduy@gmail.com>
2019-07-20 20:56:20 -0700

    Added app.use

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const { PORT } = process.env
+const { NODE_ENV, PORT } = process.env
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+if (NODE_ENV === 'development') app.use(morgan('dev'))
 
 const listener = () =>(console.log('begin'))
 app.listen(PORT, listener)
