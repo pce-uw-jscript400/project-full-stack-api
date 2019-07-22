31aeb2300dc955469c8690e662ff8cb2451bdb18
Jeremy Diaz <jeremyjdz@gmail.com>
2019-07-22 11:31:57 -0700

    Setup basic server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const { PORT } = process.env
+const { NODE_ENV, PORT } = process.env
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+if (NODE_ENV === 'development') app.use(morgan(dev))
 // body-parser
-// morgan
+
 
 const listener = () => console.log(`You're listening on PORT:5000`)
 app.listen(PORT, listener)
