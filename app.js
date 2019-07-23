4378ba8524aa61a4c82b13edd220d4c538efb6d0
slosen <sarah.losen.blair@gmail.com>
2019-07-22 20:53:56 -0700

    setup basic server

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const { PORT } = process.env
-console.log(PORT)
+const { NODE_ENV, PORT } = process.env
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-// body-parser
-// morgan
+
+
+if (NODE_ENV === 'development') app.use = express(morgan('dev'))
 
 const listener = () => console.log(`Running on PORT:${PORT}`)
 app.listen(PORT, listener)
