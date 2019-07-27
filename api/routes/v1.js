a3f2bbf9239f0db2c515b141052877f76738f0fe
Alycia Cahill <acahil@uw.edu>
2019-07-27 10:47:06 -0700

    Made a couple of fixes

--- api/routes/v1.js
+++ api/routes/v1.js
@@ -117,22 +117,21 @@ router.get('/units/:id/company/employees', async(req, res, next)=> {
     }
 })
 
-//NOT FINISHED - IF THERE IS NO EMPLOYEE WITH THAT ID IT RETURNS NULL
 //GET /api/v1/units/[id]/company/employees/[id]
 //Return the specific employee for the given company. If no company is listed, return a 404 and an appropriate
 //message. If the unit ID provided does not match a unit, return a 404 and a _different_ appropriate message.
 //If no employee with that ID exists, return a _different_ appropriate message.
 router.get('/units/:id/company/employees/:eid', async(req, res, next)=> {
-    const status = 201
+    let status = 201
     try{
         const unit = await Units.findOne({_id: req.params.id})
         const theCompany = unit.company
         const theEmployee = theCompany.employees.id({_id: req.params.eid})
-        //if (theEmployee === null){
-            //status = 404
-            //const response = "No employee with the ID found"
-            //res.json({status, response})
-       // }
+        if (theEmployee === null){
+            status = 404
+            const response = "No employee with the ID found"
+            return res.json({status, response})
+        }
         const response = theEmployee
         res.json({status, response})
     }catch(error){
@@ -154,21 +153,20 @@ router.get('/units/:id/company/employees/:eid', async(req, res, next)=> {
 })
 
 
-//WILL NOT SHOW VALIDATION ERRORS - Just shows req.body
 //POST /api/v1/units/[id]/company/employees
 //Create a new employee and return that employee for the given company. If no company is listed,
 //return a 404 and an appropriate message. If the unit ID provided does not match a unit,
 // return a 404 and a _different_ appropriate message. If the employee information is malformed in any way,
 //return a 400 and an error message with as much detail as possible.
 router.post('/units/:id/company/employees/', async (req, res, next)=> {
-    const status = 201
+   let status = 201
     try{
         const unit = await Units.findOne({_id: req.params.id})
         const theCompany = unit.company
         const newEmployee = theCompany.employees.push(req.body)
         const response = req.body
         newEmployee.isNew;
-        unit.save()
+        await unit.save()
         res.json({status, response})
     }catch(error){
         console.error(`REALERROR:/${error}`)
@@ -193,7 +191,6 @@ router.post('/units/:id/company/employees/', async (req, res, next)=> {
     }
 })
 
-///NOT FINISHED - Will not show validation errors
 // PATCH /api/v1/units/[id]/company/employees/[id]
 //Update an employee and return that employee for the given company. If no company is listed, return a 404
 //and an appropriate message. If the unit ID provided does not match a unit,
@@ -207,7 +204,7 @@ router.patch('/units/:id/company/employees/:eid', async (req, res, next)=> {
         const theCompany = unit.company
         const employee = theCompany.employees.id({_id: req.params.eid})
         employee.set(req.body)
-        unit.save()
+        await unit.save()
         const response = employee
         res.json({status, response})
     }catch(error){
@@ -274,14 +271,32 @@ router.delete('/units/:id/company/employees/:eid', async (req, res, next)=> {
 //For example, the above search would return companies with the name of "Google" and "Ooga".
 // Do not return any unit information. If no companies are found, return an empty array.
 
+//GET /api/v1/companies?employees_lte=[integer]
+// e.g. `GET http://localhost:5000/api/v1/companies?employees_lte=50`
+//Return all companies with all their employees where the number of employees is less than or equal to
+//the given integer. Do not return any unit information. If no companies are found, return an empty array.
+
+// GET /api/v1/companies?employees_gte=[integer]
+// `GET http://localhost:5000/api/v1/companies?employees_gte=100`
+//Return all companies with all their employees where the number of employees is greater than or equal to
+//the given integer. Do not return any unit information. If no companies are found, return an empty array.
+
 router.get('/companies', async(req, res, next)=> {
     const status = 201
     try{
-        const companyName = req.query.name
-        const response = await Units.find({'company.name':{$regex: new RegExp(companyName), '$options': 'i'}})
-        .select('-_id company');
+        const employees_lte = req.query.employees_lte
 
-        res.json({status, response});
+        if (employees_lte){
+            const unit = await Units.find()
+            //const company = unit.company
+            const reponse = unit
+            res.json({status, response});
+        }else{
+            const companyName = req.query.name
+            const response = await Units.find({'company.name':{$regex: new RegExp(companyName), '$options': 'i'}})
+            .select('-_id company');
+            res.json({status, response});
+        }
 
     }catch(error){
         console.log(`REALERR:${error}`)
@@ -290,6 +305,22 @@ router.get('/companies', async(req, res, next)=> {
         next(e)
     }
 });
+
+
+// GET /api/v1/employees?name=[partial-query]
+// e.g. `GET http://localhost:5000/api/v1/employees?name=ada`
+//Return all employees based off of the partial search string. The search should be for their full name.
+//For example, the above search would return the following employees:
+// - Adaline Smith
+//- Surya Adana
+//- Augusta "Ada" King-Noel
+//Do not return any unit or company information. If no employees are found, return an empty array.
+
+// GET /api/v1/employees?birthday=[date]
+// GET http://localhost:5000/api/v1/employees?birthday=[date]`
+// Return all employees who have a birthday on the given date. Do not return any unit or company information. I
+// If no employees are found, return an empty array.
+
 
 
 //Add the units
