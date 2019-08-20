f439ab14cee5805e1232fcae87c0153e3bc47c59
[jiansorge] <[ji@nsorge.com]>
2019-08-19 20:59:20 -0700

    fixed :unitId req.param in app.js

--- api/routes/v1/units.js
+++ api/routes/v1/units.js
@@ -1,24 +1,6 @@
 const router = require('express').Router()
 const Units = require('../../models/v1/units')
 
-// {
-// 	"kind": "small_office",
-// 	"floor": 5,
-// 	"special_monthly_offer": 5000000,
-// 	"company": {
-// 		"name": "Web Developers R Us",
-// 		"contact_email": "help@webdevelopersrus.com",
-// 		"employees": [{
-//       "first_name": "Webster",
-//       "last_name": "D'Veloper",
-//       "preferred_name": "Webby",
-//       "position": "Dictionary Writer",
-//       "birthday": "1900-01-30",
-//       "email": "wdveloper@webdevelopersrus.com"
-//     }]
-// 	}
-// }
-
 // GET http://localhost:5000/api/v1/units
 // GET http://localhost:5000/api/v1/units?kind=desk
 // GET http://localhost:5000/api/v1/units?floor=2
@@ -74,47 +56,5 @@ router.patch('/:id', async (req, res, next) => {
     }
   }
 })
-
-// router.put('/:id', async (req, res, next) => {
-//   const status = 200
-//   const response = await Books.findOneAndUpdate(
-//     {_id: req.params.id,}, 
-//     {
-//       title: req.body.title,
-//       published: req.body.published,
-//       authors: req.body.authors
-//     }, 
-//     {new: true},
-//     () => {
-//       const message = `Could not find fruit with ID of ${id}`
-//       next({ status: 404, message })
-//     }
-//   )
-//   res.json({ status, response })
-// })
-
-// router.delete('/:id', async (req, res, next) => {
-//   const status = 200
-//   const response = await Books.findOneAndDelete({_id: req.params.id})
-
-
-//   res.json({ status, response })
-// })
-
-// router.post('/', async (req, res, next) => {
-//   const status = 201
-//   try {
-//     const response = await Books.create(
-//       req.body
-//     )
-  
-//     res.json({ status, response })
-//   }catch(error ) {
-//       console.error(error)
-//       const e = new Error('Bad request')
-//       e.status = 400
-//       next (e)
-//     }
-// })
 
 module.exports = router
